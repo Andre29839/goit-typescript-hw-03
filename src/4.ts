5f7dd38bbc1173e0ae3961390d5de1b22eb2839d
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-09-23 15:16:50 +0300

    commit

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
+class Key {
+  private signature: number = Math.random();
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: true | false;
+  public key: Key;
+  private tenants: Person[];
+  public comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    } else {
+      console.log("Invalid key");
+    }
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new MyHouse();
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
